02539c3c776f1699420aa25bafe4a5e164108b3c
Salman <salmantnv@outlook.com>
2021-06-14 17:29:17 +0200

    updated contacts

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,68 +1,98 @@
-import React from 'react';
+import React from "react";
+import emailjs from "emailjs-com";
+
 
 const Contacts = () => {
-    return (
-<div className = "contacts">
-        <div className = "text-center">
-            <h1>Contacts</h1>
-            <p>Please insert you info and describe briefly your project. l will contact you as soon as possible</p>
-        </div>
-    <div className = "container">
-        <div className = "row">
-            <div className = "col-md-6 col-xs-12">
-                {/** Name Input */}
-                <input 
-                id = "name"
-                type = "text"
-                className = "form-control"
-                placeholder = "Name"
-                name = "name"
-                />
 
-                {/** Number Input */}
-                <input 
-                id = "number"
-                type = "text"
-                className = "form-control"
-                placeholder = "Number"
-                number = "number"
-                />
 
-                {/** Email Input */}
-                <input 
-                id = "email"
-                type = "email"
-                className = "form-control"
-                placeholder = "Email"
-                email = "email"
-                />
 
-                {/** Subject Input */}
-                <input 
-                id = "subject"
-                type = "text"
-                className = "form-control"
-                placeholder = "Subject"
-                subject = "subject"
+
+  return (
+    <div className="contacts">
+      <div className="text-center">
+        <h1>contact me</h1>
+        <p>Please fill out the form and describe you project needs and I'll contact you as soon as possible.</p>
+        
+      </div>
+      <div className="container">
+        <form>
+          <div className="row">
+            <div className="col-md-6 col-xs-12">
+              {/* NAME INPUT */}
+                <div className="text-center">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Name"
+                  name="name"
+              
                 />
+                <div className="line"></div>
+              </div>
+            
+              
+              {/* PHONE INPUT */}
+              <div className="text-center">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Phone Number"
+                  name="phone"
+                
+                />
+                <div className="line"></div>
+              </div>
+              
+                
+              
+              {/* EMAIL INPUT */}
+              <div className="text-center">
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Email"
+                  name="email"
+                  
+                />
+                <div className="line"></div>
+              </div>
+             
+              
+              
+              {/* SUBJECT INPUT */}
+              <div className="text-center">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Subject"
+                  name="subject"
+                 
+                />
+                <div className="line"></div>
+              </div>
+              
+             
+              
             </div>
-
-            <div className = "col-md-6 col-xs-12">
-                <textarea 
-                className = "form-control"
-                placeholder="Please describe your project here"
-                email = "email"
-                type = "text"
-                >
-                </textarea>
-                <button className= "contact-btn" type="submit">
-                    Contact me
-                </button>
+            <div className="col-md-6 col-xs-12">
+              {/* DESCRIPTION */}
+              <div className="text-center">
+                <textarea
+                  type="text"
+                  className="form-control"
+                  placeholder="Please describe shortly you project..."
+                  name="description"
+                
+                ></textarea>
+                <div className="line"></div>
+              </div>
+             <button className="btn-main-offer contact-btn" type="submit">contact me</button>
             </div>
-        </div>
+          </div>
+        </form>
+      </div>
     </div>
-</div>
-    )
+  )
 }
 
 export default Contacts;
